69c24598a80bbb2e9f287294d379d9e5add99c8e
MARVIN-NEW <mnyaranga44@gmail.com>
2025-04-04 04:46:49 +0300

    changes by backend developer

--- hello.sh
+++ hello.sh
@@ -1,3 +1,4 @@
 echo "hello welcome to my code"
 echo "This is my second change"
+echo "This is a change on branch2 by backend developer"
 # git init :  create a .git folder to keep track of changes
